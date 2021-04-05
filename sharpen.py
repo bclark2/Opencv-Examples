2b44284b047f5eabdbf4b3922de7e205c8191898
Bryan Clark <bryan@aristocrat.ai>
2021-04-05 16:30:45 -0700

    Create sharpen.py

---
+++ sharpen.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+import os
+import math
+
+
+def convert_size(size_bytes):
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+    return "%s %s" % (s, size_name[i])
+
+
+raw_image = cv2.imread('/home/steve/Desktop/blur.jpg')
+# uncomment if you want to see the original
+# cv2.imshow('Original', img)
+
+# cv2.resize(image, (width, height))
+img = cv2.resize(raw_image, (500, 800))
+
+# generate kernels
+kernel_sharpen_1 = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+kernel_sharpen_2 = np.array([[1, 1, 1], [1, -7, 1], [1, 1, 1]])
+kernel_sharpen_3 = np.array([[-1, -1, -1, -1, -1],
+                             [-1, 2, 2, 2, -1],
+                             [-1, 2, 8, 2, -1],
+                             [-1, 2, 2, 2, -1],
+                             [-1, -1, -1, -1, -1]]) / 8.0
+
+# applying different kernels to the input image 
+output_1 = cv2.filter2D(img, -1, kernel_sharpen_1)
+output_2 = cv2.filter2D(img, -1, kernel_sharpen_2)
+output_3 = cv2.filter2D(img, -1, kernel_sharpen_3)
+
+cv2.imshow('Sharpening', output_1)
+cv2.imshow('Excessive Sharpening', output_2)
+cv2.imshow('Edge Enhancement', output_3)
+
+filename = '/home/steve/Desktop/sharpen_images/output_3.png'
+
+cv2.imwrite(filename, output_3)
+
+k = cv2.waitKey(0)
+if k == 27:  # wait for ESC key to exit
+    cv2.destroyAllWindows()
